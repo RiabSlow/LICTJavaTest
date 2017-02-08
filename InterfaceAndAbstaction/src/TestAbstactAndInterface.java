f74448fc3e27a3b82eef0b78aebcf4fefbb05533
Asfaque Ahmed <s.poison6000@gmail.com>
2017-02-08 16:48:40 +0600

    Initial

--- InterfaceAndAbstaction/src/TestAbstactAndInterface.java
+++ InterfaceAndAbstaction/src/TestAbstactAndInterface.java
@@ -2,7 +2,8 @@
  * Created by Sakkar on 2/8/2017.
  */
 public class TestAbstactAndInterface {
-    //using interface where is no override method
+    //The main difference between the abstract class and interface is in abstract class there must be an abstract method.
+    //in interface the methods can never have a body.
     public static void main(String[] args){
         InterfaceClass interfaceClass=new TestClass();
         System.out.println("Proect name :"+interfaceClass.projectName());
